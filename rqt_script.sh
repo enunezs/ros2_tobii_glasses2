cb0df2622ad7c68b5137fccfd1c35196164c730d
Emanuel <Emanuel>
2022-08-15 15:21:12 +0100

    rviz2 script done

---
+++ rqt_script.sh
@@ -0,0 +1,7 @@
+xhost +local:root
+
+
+docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --privileged --net=host  osrf/ros:foxy-desktop rviz2
+export containerId=$(docker ps -l -q)
+
+
