5a3c99e74a48fddc6209cdd264df19f613d33697
Emanuel <Emanuel>
2022-08-15 15:10:07 +0100

    dockerscript fixed, pending one for rviz2

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,8 +1,8 @@
-export containerId=$(docker ps -l -q)
-docker run -it \                       
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"\
-    --net=host --privileged\
-    osrf/ros:foxy-desktop
+#export containerId=$(docker ps -l -q)
+docker image build -t ros2-glass-base .     
+
+docker run -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --net=host -v /home/ema/Documents/ROS2_Workspaces/:/home/ema/workspaces --privileged ros2-glass-base
+
+
+
 
